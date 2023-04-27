7c58b2372db1ca9b1746448fefd5d6b8b75af546
Igor <igoraraujobda@gmail.com>
2023-04-27 14:45:55 -0300

    requisito 3 e 4 - inserção de novos produtos

--- src/controllers/productControler.js
+++ src/controllers/productControler.js
@@ -17,7 +17,7 @@ const insertProduct = async (req, res) => {
   const newProduct = req.body;
   const result = await productService.insertProduct(newProduct);
   if (result.type) return res.status(result.type).json({ message: result.message });
-  return res.status(result.type).json(result.message);
+  return res.status(201).json(result.message);
 };
 
 module.exports = { getAll, getById, insertProduct };
